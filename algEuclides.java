bd15e85e139e32b0ff033579fa411533fc2f4fa6
Milton-Carvalhaes <milton.carvalhaes40@gmail.com>
2021-08-09 20:46:23 -0300

    Primeiro commit

---
+++ algEuclides.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class algEuclides{
+
+  public static int euclides(int num1, int num2){
+    int maior = num1, menor = num2;
+    if(num2 > num1){
+      maior = num2; menor = num1;
+    }
+    int aux = menor;
+    while(aux>0){
+      menor = aux;
+      aux = maior%aux;
+      maior = menor;
+    }
+    return menor;
+  }
+
+  public static void main(String[] args){
+    Scanner entrada = new Scanner(System.in);
+
+    int num1, num2;
+
+    System.out.print("Insira o primeiro numero: ");
+    num1 = entrada.nextInt();
+
+    System.out.print("Insira o segundo numero: ");
+    num2 = entrada.nextInt();
+
+    System.out.print(euclides(num1, num2));
+  }
+}
