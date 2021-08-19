858a0f18acb2ab4e676fdbf6853eb9dfc3fff2c7
Milton-Carvalhaes <milton.carvalhaes40@gmail.com>
2021-08-19 18:12:07 -0300

    Testando propriedade dos ímpares q minha cabeça n vai tão longe

--- impares.java
+++ impares.java
@@ -1,8 +1,8 @@
 public class impares{
 	public static void main(String[] args){
-		int i = 0;
+		int i = 1;
 		while(i<100){
-			System.out.println(i*2 + 1);
+			System.out.println(i*2 - 1);
 			i++;
 		}
 	}
